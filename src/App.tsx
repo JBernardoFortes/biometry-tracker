b8e98a3a190df4c5f5ca63124e5e348bb2812bf7
bernardo <joseberk2@gmail.com>
2026-07-01 09:55:41 -0300

    feat: configure initial routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
+import Sidebar from "./components/layout/Sidebar";
+import MainLayout from "./components/layout/MainLayout";
+import ClassesPage from "./pages/ClassesPage";
+import ClassPage from "./pages/ClassPage";
+import RegisterBiometry from "./pages/RegisterBiometry";
+
+import { createBrowserRouter } from "react-router-dom";
+
+createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <ClassesPage></ClassesPage>
+      },
+      {
+        path: "/class/:classId",
+        element: <ClassPage></ClassPage>
+      }
+      , {
+        path: "/class/:classId/register-fingerprint"
+        , element: <RegisterBiometry></RegisterBiometry>
+      }
+
+    ]
+  },
+]);
 
 function App() {
-  return <div className="text-3xl text-amber-400"> oi </div>
+  return (
+    <div className="text-3xl text-amber-400">
+      <Sidebar></Sidebar>
+      <MainLayout>
+      </MainLayout>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/RegisterBiometry.tsx
@@ -0,0 +1,8 @@
+
+const RegisterBiometry = () => {
+  return (
+    <div>RegisterBiometry</div>
+  )
+}
+
+export default RegisterBiometry
