7eee566fa10113aefbfed161e269706ab06c05b1
Bernardo <joseberk2@gmail.com>
2026-07-08 14:35:34 -0300

    refactor: remove pages not used from app and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
 import MainLayout from "./components/layout/MainLayout";
 
 import ClassPage from "./pages/ClassPage";
-import ClassesPage from "./pages/ClassesPage";
 
 const router = createBrowserRouter([
   {
@@ -16,19 +15,11 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Navigate to="/classes" replace />,
-      },
-      {
-        path: "class/:id",
         element: <ClassPage />,
       },
       {
-        path: "classes",
-        element: <ClassesPage />,
-      },
-      {
-        path: "/",
-        element: <Navigate to="/classes" replace />,
+        path: "*",
+        element: <Navigate to="/" replace />,
       },
     ],
   },
