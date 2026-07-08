862cf2943cf52dadc7bb98d5a7b03225a70a2c6b
Joao Carlos <joaokarlos1951@gmail.com>
2026-07-08 10:15:54 -0300

    feat/segundo-endpoint

--- backend/app/models/evento.py
+++ backend/app/models/evento.py
@@ -4,5 +4,4 @@ from pydantic import BaseModel
 
 class EventoRequest(BaseModel):
     evento: str
-    biometriaId: Optional[int] = None
-    distancia: Optional[int] = None
+    biometriaId: int
--- backend/app/routers/eventos.py
+++ backend/app/routers/eventos.py
@@ -1,11 +1,12 @@
 from app.models.evento import EventoRequest
+from typing import Optional
 from app.storage.memoria import (
     adicionar_evento,
     listar_eventos,
     limpar_eventos
 )
 
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 
 router = APIRouter()
 
@@ -17,9 +18,25 @@ def receber_evento(evento: EventoRequest):
         "mensagem": "Evento recebido com sucesso."
     }
 
+@router.post("/evento/imagem")
+async def receber_imagem(imagem: UploadFile = File(...)):
+    return {
+        "mensagem": "Imagem recebida com sucesso.",
+        "nome_arquivo": imagem.filename,
+        "tipo": imagem.content_type
+    }
+
 @router.get("/eventos")
-def obter_eventos():
-    return listar_eventos()
+def obter_eventos(evento: Optional[str] = None):
+    eventos = listar_eventos()
+
+    if evento:
+        eventos = [
+            e for e in eventos
+            if e["evento"] == evento
+        ]
+
+    return eventos
 
 @router.delete("/eventos")
 def apagar_eventos():
