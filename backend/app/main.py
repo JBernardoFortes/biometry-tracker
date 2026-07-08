3c529959fdf47d88e5aeff873e458de08421dc8c
Bernardo <joseberk2@gmail.com>
2026-07-08 14:20:20 -0300

    refactor: add CORS permission

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from app.routers.eventos import router as eventos_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Biometry Tracker API",
     description="Integração entre o hardware e o frontend do sistema de frequência por biometria.",
-    version="1.0.0"
+    version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5174"],
+)
 app.include_router(eventos_router)
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
