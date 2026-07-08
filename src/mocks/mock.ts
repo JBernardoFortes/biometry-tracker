8e9e08f868e45b467084fe7fb9789504fb0e43c5
Bernardo <joseberk2@gmail.com>
2026-07-08 19:06:00 -0300

    refactor: change names in data mock

--- src/mocks/mock.ts
+++ src/mocks/mock.ts
@@ -1,8 +1,8 @@
 import type { Aluno } from "../types";
 
 export const alunosCadastrados: Aluno[] = [
-  { id: 1, matricula: "2026001", nome: "Ana Souza" },
-  { id: 2, matricula: "2026002", nome: "Bruno Lima" },
-  { id: 3, matricula: "2026003", nome: "Carla Dias" },
-  { id: 4, matricula: "2026004", nome: "Diego Alves" },
+  { id: 1, matricula: "2026001", nome: "Artur Vinicius" },
+  { id: 2, matricula: "2026002", nome: "Jose Bernardo" },
+  { id: 3, matricula: "2026003", nome: "Joao Carlos" },
+  { id: 4, matricula: "2026004", nome: "Jose Luis" },
 ];
